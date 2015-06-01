f3cd928a362bb4eb599787d5f1dadc8e8bea3fde
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-05-31 23:32:43 -0500

    Initial untested project file and basic top-level VHDL file

---
+++ Basys2Project.vhd
@@ -0,0 +1,27 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity Basys2Project is
+  port (
+    mclk     : in    std_logic; 
+    uclk     : in    std_logic; 
+    btn      : in    std_logic_vector (3 downto 0); 
+    sw       : in    std_logic_vector (7 downto 0); 
+    led      : out   std_logic_vector (7 downto 0); 
+    seg      : out   std_logic_vector (6 downto 0); 
+    an       : out   std_logic_vector (3 downto 0); 
+    dp       : out   std_logic; 
+    OutBlue  : out   std_logic_vector (2 downto 1); 
+    OutGreen : out   std_logic_vector (2 downto 0); 
+    OutRed   : out   std_logic_vector (2 downto 0); 
+    HS       : out   std_logic; 
+    VS       : out   std_logic
+  );
+
+end Basys2Project;
+
+architecture Structural of Basys2Project is
+begin
+end Structural;
