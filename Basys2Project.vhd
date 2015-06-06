f252bb1deba6929d745d9a9ae21a94a6ddb7567b
Thomas Russell Murphy <thomas.russell.murphy@case.edu>
2015-06-05 23:40:02 -0500

    Remove trailing whitespace, add comments to port list

--- Basys2Project.vhd
+++ Basys2Project.vhd
@@ -5,20 +5,26 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity Basys2Project is
   port (
-    mclk     : in    std_logic; 
-    uclk     : in    std_logic; 
-    btn      : in    std_logic_vector (3 downto 0); 
-    sw       : in    std_logic_vector (7 downto 0); 
-    led      : out   std_logic_vector (7 downto 0); 
-    seg      : out   std_logic_vector (6 downto 0); 
-    an       : out   std_logic_vector (3 downto 0); 
-    dp       : out   std_logic; 
-    OutBlue  : out   std_logic_vector (2 downto 1); 
-    OutGreen : out   std_logic_vector (2 downto 0); 
-    OutRed   : out   std_logic_vector (2 downto 0); 
-    HS       : out   std_logic; 
-    VS       : out   std_logic
+  	-- Clock inputs
+    mclk     : in    std_logic;
+    uclk     : in    std_logic;
+    -- User physical inputs
+    btn      : in    std_logic_vector (3 downto 0);
+    sw       : in    std_logic_vector (7 downto 0);
+    -- User LED/Display outputs
+    led      : out   std_logic_vector (7 downto 0);
+    seg      : out   std_logic_vector (6 downto 0);
+    an       : out   std_logic_vector (3 downto 0);
+    dp       : out   std_logic;
+    -- VGA Interface
+    OutBlue  : out   std_logic_vector (2 downto 1);
+    OutGreen : out   std_logic_vector (2 downto 0);
+    OutRed   : out   std_logic_vector (2 downto 0);
+    HSYNC    : out   std_logic;
+    VSYNC    : out   std_logic
     -- TODO: add the rest of the standard Basys2 signals to the port map
+    -- PS2
+    -- Expansion headers (6-pin, 4 data connections each)
   );
 
 end Basys2Project;
